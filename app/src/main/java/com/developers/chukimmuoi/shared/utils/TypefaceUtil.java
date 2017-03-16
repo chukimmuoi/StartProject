404a8fde288b7cc3c5fe74481b5f4317227078c7
chukimmuoi <chukimmuoi@gmail.com>
2017-03-17 06:28:45 +0700

    Add fonts size

--- app/src/main/java/com/developers/chukimmuoi/shared/utils/TypefaceUtil.java
+++ app/src/main/java/com/developers/chukimmuoi/shared/utils/TypefaceUtil.java
@@ -1,7 +1,10 @@
 package com.developers.chukimmuoi.shared.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
+import android.util.DisplayMetrics;
 
 import java.lang.reflect.Field;
 
@@ -17,6 +20,8 @@ import java.lang.reflect.Field;
 
 public class TypefaceUtil {
     /**
+     * see https://gist.github.com/artem-zinnatullin/7749076
+     * <p>
      * Using reflection to override default typeface
      * NOTICE: DO NOT FORGET TO SET TYPEFACE FOR APP THEME AS DEFAULT TYPEFACE WHICH WILL BE OVERRIDDEN
      *
@@ -35,6 +40,28 @@ public class TypefaceUtil {
                     = Typeface.class.getDeclaredField(defaultFontNameToOverride);
             defaultFontTypefaceField.setAccessible(true);
             defaultFontTypefaceField.set(null, customFontTypeface);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * see http://stackoverflow.com/questions/6786439/how-to-set-font-scale-in-my-own-android-application
+     * <p>
+     * Using reflection to override default text size
+     *
+     * @param context  MainActivity, place it before calling super.setContentView(R.layout.yourLayout)
+     * @param textSize for example 0.85f small, 1.0f normal, 1.15f large
+     */
+    public static void overrideSize(Activity context, float textSize) {
+        try {
+            Configuration configuration = context.getResources().getConfiguration();
+            configuration.fontScale = textSize;
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            context.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.fontScale * metrics.density;
+            context.getBaseContext().getResources().updateConfiguration(configuration, metrics);
         } catch (Exception e) {
             e.printStackTrace();
         }
